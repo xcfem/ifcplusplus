8599e702a3f3b1d6c76e63d40700870ee0da885e
Bernd Hahnebach <bernd@bimstatik.org>
2017-12-28 13:47:32 +0100

    simple viewer, set main window title

--- SimpleViewerExample/src/gui/MainWindow.cpp
+++ SimpleViewerExample/src/gui/MainWindow.cpp
@@ -36,7 +36,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OU
 MainWindow::MainWindow( IfcPlusPlusSystem* sys, ViewerWidget* vw, QWidget *parent) : m_system(sys), m_viewer_widget(vw), QMainWindow(parent)
 {
 	m_system = sys;
-	setWindowTitle("IfcQuery example application");
+	setWindowTitle("IfcQuery example application -- based on Fabian Gerolds GitHub commit 169 -- 318b2511 Merge pull request #102 from berndhahnebach/gccfixes");
 	setWindowIcon( QIcon( ":img/IfcPlusPlusViewerWindowIcon.png" ) );
 	
 	// global style sheet definitions
